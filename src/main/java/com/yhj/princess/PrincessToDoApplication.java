444b51078fd647d7a3ddd6cf20c37f6c9346c1d4
yhj <dbgowjd98@gmail.com>
2025-08-12 21:15:39 +0900

    first

---
+++ src/main/java/com/yhj/princess/PrincessToDoApplication.java
@@ -0,0 +1,13 @@
+package com.yhj.princess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincessToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincessToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yhj/princess/PrincessToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhj.princess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincessToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
